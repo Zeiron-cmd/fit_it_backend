68b0eefea2289344ecc974ceffd32b45781dd941
Zeiron-cmd <timuvasilij@gmail.com>
2026-05-09 18:47:11 +0300

    feat: add refresh token and cors

--- app/main.py
+++ app/main.py
@@ -9,9 +9,24 @@ from app.routers.auth import router as auth_router
 from app.routers.profile import router as profile_router
 from app.routers.meals import router as meals_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Fit it Backend")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
